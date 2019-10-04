75d99be92e2364774c1ca16ae779ac0c0735acb4
Alisson Kendi Kohatsu <kohatsukendi@gmail.com>
2019-10-04 13:23:06 -0300

    Connecting in real time with WebSocket

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,12 +1,27 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import { Link } from 'react-router-dom';
+import socketClient from 'socket.io-client';
 import api from '../../servicies/api';
+
 
 import './styles.css';
 
 export default function Dashboard() {
 
     const [spots, setSpots] = useState([]);
+    const [requests, setRequests] = useState([]);
+
+    const user_id = localStorage.getItem("user");
+    const socket = useMemo(() => socketClient('http://192.168.0.106:3030', {
+        query: { user_id }
+    }), [user_id]);
+
+
+    useEffect(() => {
+        socket.on('booking_request', data => {
+            setRequests([...requests, data]);
+        });
+    }, [requests, socket]);
 
     useEffect(() => {
         async function loadSpots() {
@@ -14,14 +29,35 @@ export default function Dashboard() {
             const response = await api.get("/dashboard", {
                 headers: { user_id }
             });
-            console.log(response.data);
             setSpots(response.data);
         }
         loadSpots();
     }, []);
 
+    async function handleAccept(id) {
+        await api.post(`/bookings/${id}/approvals`);
+        setRequests(requests.filter(request => request._id !== id));
+    }
+
+    async function handleReject(id) {
+        await api.post(`/bookings/${id}/rejections`);
+        setRequests(requests.filter(request => request._id !== id));
+    }
+
     return (
         <>
+            <ul className="notifications">
+                {requests.map(request => (
+                    <li key={request._id}>
+                        <p>
+                            <strong>{request.user.email}</strong> está solicitando uma reserva em <strong>{request.spot.company}</strong> para a data: <strong>{request.date}</strong>.
+                        </p>
+                        <button className="accept" onClick={() => handleAccept(request._id)}>Aceitar</button>
+                        <button className="reject" onClick={() => handleReject(request._id)}>Rejeitar</button>
+                    </li>
+                ))}
+            </ul>
+
             <ul className="spot-list">
                 {spots.map(spot => (
                     <li key={spot._id}>
